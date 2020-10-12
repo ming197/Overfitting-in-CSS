c6c4ad0856e5b46d9c1d7d3a551db37c7f104bb6
ming197 <ming197@qq.com>
2020-10-12 21:09:03 +0800

    v

--- Experiment/Stock-market-forecasting/data/data_scraping.py
+++ Experiment/Stock-market-forecasting/data/data_scraping.py
@@ -1,82 +1,101 @@
 import quandl
-import os
+import os, sys
 import pandas as pd
 from datetime import datetime
 import numpy as np
 import calendar
+import time
+import threading
 
 
+
+path = "./data_csv"
 file_exceptions = "exceptions.txt"
 tickers = pd.read_csv('SPXconst.csv')
 
 
 API_KEY = ['95NJZYJYyXToregziQw8', 'yJAooAzza7L4EJxyFkcs', '2H5CLCnZE_AUSAzUhicw', 'KhALVVzAJx-FK-XsFxvS', 'jMVcgK1aP-MUmyKChg9-']
+api_index = 0
+data = None # 股票的开盘数据和收盘数据
+calls_limit = 50000
+calls_num = [0 for x in API_KEY]
+calls_flag = [False for x in API_KEY]
+shutdown = False
 
 # 获取股票的开盘价和收尾价
+    
 def getInfo(ticker, start_date, end_date):
+    # 声明全局变量
+    global api_index, shutdown, data
     # API调用数据
-    data = quandl.get("WIKI/{}".format(ticker.replace(".","_")), start_date=start_date, end_date=end_date, api_key=API_KEY)
+    for i in range(len(API_KEY)):
+        if calls_flag[api_index] == True:
+            api_index = (api_index + 1) % len(API_KEY)
+        else:
+            break
+        shutdown = (shutdown and calls_flag[api_index])
+
+    if(shutdown == True):
+        return None
+
+    calls_num[api_index] += 1
+    if(calls_num[api_index] == calls_limit):
+        calls_flag[api_index] = True
+
+    data = quandl.get("WIKI/{}".format(ticker.replace(".","_")), start_date=start_date, end_date=end_date, api_key=API_KEY[api_index])
     data = data.loc[:, ['Open', 'Close']]
-
-    # 全部转换为DataFrame数据类型，避免拼接时发生错误
-    data_open = pd.DataFrame(data['Open'].rename(ticker, inplace=True))
-    data_close = pd.DataFrame(data['Close'].rename(ticker, inplace=True))
-
-    return data_open, data_close
+    api_index = (api_index + 1) % len(API_KEY)
+    time.sleep(1)
 
 
 def main():
-
-    path = "data_monthly"
-    
-
+    global data
     years = [str(x) for x in range(1990, 2019)]
-
     for year in years:
-        tickers_open = pd.DataFrame()
-        tickers_close = pd.DataFrame()
         months = [("{:02d}".format(x) + '/' + year) for x in range(1, 13)]
-
         for month in months:
             # 获取日期信息，以及所有的股票名称
             month_str = month
             month = datetime.strptime(month, "%m/%Y") # 转换成时间戳
             monthFirstDay = datetime(month.year, month.month, 1).strftime(('%Y-%m-%d'))
             monthLastDay = datetime(month.year, month.month, calendar.monthrange(month.year, month.month)[1]).strftime(('%Y-%m-%d'))
-            tickers_month =tickers[month_str].dropna().tolist()
+            tickers_month = tickers[month_str].dropna().tolist()
+            month_str = '-'.join(month_str.split('/')[::-1])
 
-            # 获得每个月每支股票的信息， index为日期， cols为股票名称
-            month_open = pd.DataFrame()
-            month_close = pd.DataFrame()
             for ticker in tickers_month:
-                try:
-                    data_open, data_close = getInfo(ticker, monthFirstDay, monthLastDay)
-                    # 补NaN
-                    month_open = pd.concat([month_open, data_open], join='outer', axis=1)
-                    month_close = pd.concat([month_close, data_close], join='outer', axis=1)
-                    print("{} in {} finished!".format(ticker, month_str))
+                try: 
+                    runtimes = 0
+                    flag = False
+                    while(runtimes < len(API_KEY) and not flag):
+                        data = None
+                        
+                        t = threading.Thread(target = getInfo(ticker, monthFirstDay, monthLastDay))
+                        t.setDaemon(True)
+                        t.start()
+                        t.join(60)
+                        print("###")
+                        print(data)
+                        if(shutdown == True):
+                            return
+                        if data == None:
+                            runtimes += 1
+                        else:
+                            flag = True
+                            # 将数据写入csv文件
+                            data.to_csv("{}/{} {}.csv".format(path, month_str, ticker))
+                            print("{} in {} finished!".format(ticker, month_str))
                 except:
                     with open(file_exceptions, 'a') as f:
                         f.write("{} in {} not exist!\n".format(ticker, month_str))
                     print("{} in {} not exist!".format(ticker, month_str))
-                 
             print("Infomation in {} finished!".format(month_str))
-            month_open.index.name = month_close.index.name = 'Date'
-            month_open.to_csv(path + "/Open-{}.csv".format('-'.join(month_str.split('/')[::-1])))
-            month_close.to_csv(path + "/Close-{}.csv".format('-'.join(month_str.split('/')[::-1])))
+        
 
-            # 将一年中所有月份的股票信息合并， 空缺处为NaN 
-            tickers_open = pd.concat([tickers_open, month_open], join='outer', axis=0)
-            tickers_close = pd.concat([tickers_close, month_close], join='outer', axis=0)
             
-            
-                
-            
-        # 存储每年的股票信息
-        tickers_open.index.name = tickers_close.index.name = 'Date'
-        tickers_open.to_csv("Open-{}.csv".format(year))
-        tickers_close.to_csv("Close-{}.csv".format(year))
-        print("S&P500 price data in {} finished!".format(year))
-
+       
 if __name__ == "__main__":
     main()
+    if(shutdown):
+        print("call nums reach limits!")
+    
+    
